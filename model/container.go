691370659b575d4c733b3bf8d8eebf7fecedf717
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2020-11-16 12:47:10 +0700

    Initialization atribute DB

--- model/container.go
+++ model/container.go
@@ -1,1 +1,8 @@
 package model
+
+type ContainerBalls struct {
+	tableName interface{} `pg:"public.container_balls"`
+	Id int `pg:"id"`
+	Size int `pg:"size"`
+	PlayerId []int `pg:"player_id,array"`
+}
--- model/tennis_player.go
+++ model/tennis_player.go
@@ -1,1 +1,7 @@
 package model
+
+type TennisPlayer struct {
+	tableName interface{} `pg:"public.tennis_players"`
+	Id int `pg:"id"`
+	PlayerName string `pg:"player_name"`
+}
